79872dfdb493ff060c6f322be2b9793548fd3c6e
Jordan Masone <jordanmasone1@gmail.com>
2024-10-31 12:37:03 -0400

    Added sprite class and reworked drawing function.

---
+++ assets/js/Sprite.js
@@ -0,0 +1,69 @@
+import Vector2 from './Vector2.js';
+
+export default class Sprite {
+	constructor({
+		resource, // Image we want to draw.
+		frameSize, // Size of the crop of the image.
+		hFrames, // How the sprite is arranged horizontally.
+		vFrames, // How the sprite is arranged vertically.
+		frame, // Which frame we want to show.
+		scale, // How large to draw this image.
+		position // Where to draw the image.
+	}) {
+		this.resource = resource;
+		this.frameSize = frameSize ?? new Vector2(16, 16);
+		this.hFrames = hFrames ?? 1;
+		this.vFrames = vFrames ?? 1;
+		this.frame = frame ?? 0;
+		this.frameMap = new Map();
+		this.scale = scale ?? 1;
+		this.position = position ?? new Vector2(0, 0);
+		this.buildFrameMap();
+	}
+
+	buildFrameMap() {
+		let frameCount = 0;
+		for (let vertically = 0; vertically < this.vFrames; vertically++) {
+			for (let horizontally = 0; horizontally < this.hFrames; horizontally++) {
+				this.frameMap.set(
+					frameCount,
+					new Vector2(
+						this.frameSize.x * horizontally,
+						this.frameSize.y * vertically
+					)
+				);
+				frameCount++;
+			}
+		}
+	}
+
+	drawSprite(ctx, x, y) {
+		if (!this.resource.isLoaded) {
+			return;
+		}
+
+		// Find the correct sprite sheet frame to use.
+		let frameCoordX = 0;
+		let frameCoordY = 0;
+		const frame = this.frameMap.get(this.frame);
+		if (frame) {
+			frameCoordX = frame.x;
+			frameCoordY = frame.y;
+		}
+
+		const frameSizeX = this.frameSize.x;
+		const frameSizeY = this.frameSize.y;
+
+		ctx.drawImage(
+			this.resource.image,
+			frameCoordX,
+			frameCoordY, // Top Y corner of frame.
+			frameSizeX, // How much to crop from the sprite sheet (X).
+			frameSizeY, // How much to crop from the sprite sheet (Y).
+			x, // Where to place this on canvas tag X (0).
+			y, // Where to place this on cavas tag Y (0).
+			frameSizeX * this.scale, // How large to scale it (X).
+			frameSizeY * this.scale // How large to scale it (Y).
+		);
+	}
+}
--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 // https://www.youtube.com/watch?v=HmxNrlPx8iY
 
 import resources from './assets/js/Resources.js';
+import Sprite from './assets/js/Sprite.js';
+import Vector2 from './assets/js/Vector2.js';
 
+// Canvas & Context
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
+// Sprites
+const sky = new Sprite({
+	resource: resources.images.sky,
+	frameSize: new Vector2(320, 180)
+});
+const ground = new Sprite({
+	resource: resources.images.ground,
+	frameSize: new Vector2(320, 180)
+});
+const heroPosition = new Vector2(16 * 6, 16 * 5);
+const hero = new Sprite({
+	resource: resources.images.hero,
+	frameSize: new Vector2(32, 32),
+	hFrames: 3,
+	vFrames: 8,
+	frame: 1
+});
+const shadow = new Sprite({
+	resource: resources.images.shadow,
+	frameSize: new Vector2(32, 32)
+});
+
+// Canvas Draw
 const draw = () => {
-	const sky = resources.images.sky;
-	if (sky.isLoaded) {
-		ctx.drawImage(sky.image, 0, 0);
-	}
+	sky.drawSprite(ctx, 0, 0);
+	ground.drawSprite(ctx, 0, 0);
+
+	// Center the Hero in the cell
+	const heroOffset = new Vector2(-8, -21);
+	const heroPositionX = heroPosition.x + heroOffset.x;
+	const heroPositionY = heroPosition.y + 1 + heroOffset.y;
+
+	shadow.drawSprite(ctx, heroPositionX, heroPositionY);
+	hero.drawSprite(ctx, heroPositionX, heroPositionY);
 };
 
+// Game Loop
 setInterval(() => {
-	console.log('draw');
+	hero.frame += 1;
 	draw();
 }, 300);
