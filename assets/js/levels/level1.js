f3b3b375f3abedb4e6a01941a000174756efd946
Jordan Masone <jordanmasone1@gmail.com>
2024-11-02 09:47:25 -0400

    Added grid-based movement and wall logic.

---
+++ assets/js/levels/level1.js
@@ -0,0 +1,28 @@
+export const walls = new Set();
+
+// Trees
+walls.add(`64,48`);
+walls.add(`208,64`);
+walls.add(`224,32`);
+
+// Squares
+walls.add(`64,64`);
+walls.add(`64,80`);
+walls.add(`80,64`);
+walls.add(`80,80`);
+walls.add(`128,48`);
+walls.add(`144,48`);
+
+// Water
+walls.add(`112,80`);
+walls.add(`128,80`);
+walls.add(`144,80`);
+walls.add(`160,80`);
+
+// Stones
+walls.add(`192,96`);
+walls.add(`208,96`);
+walls.add(`224,96`);
+
+// House
+walls.add(`224,64`);
--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import Sprite from './assets/js/Sprite.js';
 import Vector2 from './assets/js/Vector2.js';
 import GameLoop from './assets/js/GameLoop.js';
 import Input, { UP, DOWN, LEFT, RIGHT } from './assets/js/Input.js';
+import { gridCells, isSpaceFree } from './assets/js/helpers/grid.js';
+import { moveTowards } from './assets/js/helpers/moveTowards.js';
+import { walls } from './assets/js/levels/level1.js';
 
 // Canvas & Context
 const canvas = document.getElementById('game-canvas');
@@ -19,15 +22,16 @@ const ground = new Sprite({
 	resource: resources.images.ground,
 	frameSize: new Vector2(320, 180)
 });
-const heroPosition = new Vector2(16 * 6, 16 * 5);
 const input = new Input();
 const hero = new Sprite({
 	resource: resources.images.hero,
 	frameSize: new Vector2(32, 32),
 	hFrames: 3,
 	vFrames: 8,
-	frame: 1
+	frame: 1,
+	position: new Vector2(gridCells(6), gridCells(5))
 });
+const heroDestinationPosition = hero.position.duplicate();
 const shadow = new Sprite({
 	resource: resources.images.shadow,
 	frameSize: new Vector2(32, 32)
@@ -35,21 +39,44 @@ const shadow = new Sprite({
 
 // Updating entities in the game
 const update = () => {
+	const distance = moveTowards(hero, heroDestinationPosition, 1);
+
+	// Attempt to move again if the hero is at his position
+	const hasArrived = distance <= 1;
+	if (hasArrived) {
+		tryMove();
+	}
+};
+
+const tryMove = () => {
+	if (!input.direction) {
+		return;
+	}
+
+	let nextX = heroDestinationPosition.x;
+	let nextY = heroDestinationPosition.y;
+	const gridSize = 16;
+
 	if (input.direction === UP) {
-		heroPosition.y -= 1;
+		nextY -= gridSize;
 		hero.frame = 6;
 	}
 	if (input.direction === DOWN) {
-		heroPosition.y += 1;
+		nextY += gridSize;
 		hero.frame = 0;
 	}
 	if (input.direction === LEFT) {
-		heroPosition.x -= 1;
+		nextX -= gridSize;
 		hero.frame = 9;
 	}
 	if (input.direction === RIGHT) {
-		heroPosition.x += 1;
+		nextX += gridSize;
 		hero.frame = 3;
+	}
+
+	if (isSpaceFree(walls, nextX, nextY)) {
+		heroDestinationPosition.x = nextX;
+		heroDestinationPosition.y = nextY;
 	}
 };
 
@@ -60,8 +87,8 @@ const draw = () => {
 
 	// Center the Hero in the cell
 	const heroOffset = new Vector2(-8, -21);
-	const heroPositionX = heroPosition.x + heroOffset.x;
-	const heroPositionY = heroPosition.y + 1 + heroOffset.y;
+	const heroPositionX = hero.position.x + heroOffset.x;
+	const heroPositionY = hero.position.y + 1 + heroOffset.y;
 
 	shadow.drawSprite(ctx, heroPositionX, heroPositionY);
 	hero.drawSprite(ctx, heroPositionX, heroPositionY);
